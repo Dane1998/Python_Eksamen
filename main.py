c6c294933751724e94922e9b70ea1d38933160c1
daniel poulsen <daniel.poulsen@live.dk>
2021-05-25 10:34:08 +0200

    Gen/CNN

---
+++ main.py
@@ -0,0 +1,50 @@
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# Vores ImageDataGenerator udvider størrelsen af vores datasæt,
+# Ved at generere modifiserede versioner af hvert billed der er i data mappen.
+
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
+
+
+# Vi bygger her vores CNN (Convolutional Neural Network)
+# Vi bruger Sequential 
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
